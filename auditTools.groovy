ea87ae849258b33812e6b557b85130b87966523a
santosh tanna <santosh.tanna11@gmail.com>
2024-06-15 08:37:53 +0530

    mm

--- auditTools.groovy
+++ auditTools.groovy
@@ -2,7 +2,7 @@ def auditTools(Map config){
     node{
         echo "${config.message}"
         sh '''
-        git version
+        git --version
         mvn -version
         java -version
     }
